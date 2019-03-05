6c6b69af77ca36b93b3f0e4a3b8960cab6e049ac
Asmaa Zakaria <asmaazakaria94@gmail.com>
2019-03-05 17:14:08 +0200

    implementing Material-ui

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Template extends Component {
     render() {
         return (
-          <div>
-              <header>
-                  <h1>TicTacToe Game</h1>
-              </header>
-              <main>
-                  {this.props.children}
-              </main>
-          </div>
+          <MuiThemeProvider>
+              <div>
+                  <header>
+                      <h1>TicTacToe Game</h1>
+                  </header>
+                  <main>
+                      {this.props.children}
+                  </main>
+              </div>
+          </MuiThemeProvider>
         )
     }
 }
