290dffad2035e02df2f47c3a340044b1ec059925
Asmaa Zakaria <asmaazakaria94@gmail.com>
2019-03-09 14:00:37 +0200

    add profile page styling

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
+import {Container, Name, GameListHeader, GameList, GameRecord, ColumnLabels, Column} from '../styled/Profile';
 
 class Profile extends Component {
+
+    static defaultProps = {
+        user: {
+            email: 'USER_EMAIL',
+            games: [
+                {
+                    winner: true,
+                    createdAt: '09-03-2019',
+                    id: '0001'
+                },
+                {
+                    winner: true,
+                    createdAt: '08-03-2019',
+                    id: '0002'
+                },
+                {
+                    winner: true,
+                    createdAt: '07-03-2019',
+                    id: '0003'
+                }
+            ]
+        }
+    }
+
     render() {
+        let {email} = this.props.user;
+
         return (
-          <div>
-              <h2>Profile</h2>
-          </div>
+          <Container>
+              <Name>{email}</Name>
+              <GameList>
+                  <GameListHeader>My Games</GameListHeader>
+                  <ColumnLabels>
+                      <Column>Outcome</Column>
+                      <Column>Guess</Column>
+                      <Column>Guessed Correctly</Column>
+                      <Column>Date</Column>
+                  </ColumnLabels>
+              </GameList>    
+          </Container>
         )
     }
 }
